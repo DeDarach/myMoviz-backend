73e8ab2c5e99907b0fd1ae2cbe87b994adc871bd
Louis <lrenard911@gmail.com>
2026-04-22 12:37:15 +0200

    myMoviz - first deployment on Vercel.

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = '';
+const BACKEND_URL = 'https://my-moviz-backend-coral-nine.vercel.app';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
+const FRONTEND_URL = 'https://my-moviz-frontend-alpha-liart.vercel.app/';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
